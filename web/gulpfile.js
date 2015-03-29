b8513fbaea63f5ea6399465ce23bf006f8272d3b
Daniel Phan <who.reads.these@gmail.com>
2015-03-29 12:51:20 -0700

    Inject build artifact stream into app.html
    Instead of globbing dist/ to find artifacts

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,24 +1,22 @@
+var del = require("del");
 var gulp = require("gulp");
 var inject = require("gulp-inject");
 var util = require("gulp-util");
-var webpack = require("webpack");
+var webpack = require("gulp-webpack");
 var webpackDevServer = require("webpack-dev-server");
 
 var webpackConfig = require("./webpack.config");
 
-gulp.task("build", function(callback) {
-  webpack(webpackConfig, function(err, stats) {
-    if (err) {
-      throw new util.PluginError("webpack", err);
-    }
-    util.log("[build]", stats.toString());
+gulp.task("build", ["clean"], function() {
+  var dist = gulp.src(webpackConfig.entry)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest("dist/"));
 
-    // TODO Inject the build artifact stream directly,
-    // instead of finding the build artifacts by globbing.
-    var dist = gulp.src(["dist/**/*.css", "dist/**/*.js"], {read: false});
-    gulp.src("src/app.html")
-      .pipe(inject(dist, {relative: true}))
-      .pipe(gulp.dest("dist/"))
-      .on("finish", callback);
-  });
+  return gulp.src("src/app.html")
+    .pipe(inject(dist, {relative: true}))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("clean", function(callback) {
+  del(["dist/"], callback);
 });
