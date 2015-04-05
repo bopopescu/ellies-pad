bc264ab1e0f27bb78faa17152b9ee79c9ff5840a
Daniel Phan <who.reads.these@gmail.com>
2015-04-05 16:00:23 -0700

    Add tools/osx/zshrc.zsh
    Contains Mac-specific nice-to-haves

---
+++ tools/osx/zshrc.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+ZSHRC_OSX="$0:A"
+ZSHRC=${ZSHRC_OSX/\/osx\/zshrc.zsh/\/zshrc.zsh}
+
+source "$ZSHRC"
+
+antigen bundle brew
+antigen bundle brew-cask
+antigen bundle osx
+antigen bundle terminalapp
