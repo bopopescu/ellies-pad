642dc533a29f336ca4f0b9ae002d492a738430d7
Daniel Phan <who.reads.these@gmail.com>
2015-04-08 12:58:42 -0700

    Add serve.bash script
    Which runs both the AppEngine dev server and the BrowserSync server in a tmux session

---
+++ tools/common/serve.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+session="tools/common/serve"
+
+tmux has-session -t $session
+if [[ $? -ne 0 ]]; then
+  tmux new-session -s $session -d -n api 'cd "$ELLIESPATH" && goapp serve ./api'
+  tmux split-window 'cd "$ELLIESPATH/web" && npm run gulp -- serve'
+
+  tmux set-option -t $session mode-mouse on
+  tmux set-option -t $session mouse-select-window on
+  tmux set-option -t $session mouse-select-pane on
+  tmux set-option -t $session mouse-resize-pane on
+fi
+
+tmux attach-session -r -t $session
