3b30b5a02351f5eb58c245eb1c154e64ca1ff368
Daniel Phan <who.reads.these@gmail.com>
2015-04-11 22:38:01 -0700

    Use ViewMixin in App

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,14 +1,23 @@
-var color = require("color");
+//var color = require("color");
 var React = require("react");
 
-var CreateTask = require("../task/CreateTask");
+//var CreateTask = require("../task/CreateTask");
+var ViewMixin = require("../common/ViewMixin");
 
 var App = React.createClass({
+    mixins: [ViewMixin],
+
+    getStyles: function() {
+        return {
+            color: "#ff0000"
+        };
+    },
+
     render: function() {
-        return React.DOM.div({},
-            React.createElement(CreateTask, {
-                color: color("#663399").clone().lighten(1)
-            }));
+        return React.DOM.div({
+                style: this.getBuiltStyles()
+            },
+            "hello");
     }
 });
 
