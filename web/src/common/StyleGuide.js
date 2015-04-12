af8a3b700949d10cc3af40df29ffe15a076769db
Daniel Phan <who.reads.these@gmail.com>
2015-04-11 22:30:21 -0700

    Add empty StyleGuide.js for CSS constants

---
+++ web/src/common/StyleGuide.js
@@ -0,0 +1,15 @@
+//var color = require("color");
+
+// StyleGuide contains constants used to define the look and feel of Ellie's Pad.
+var StyleGuide = {
+    border: {},
+    borderRadius: {},
+    color: {},
+    fontSize: {},
+    fontFamily: {},
+    margin: {},
+    padding: {},
+    zIndex: {}
+};
+
+module.exports = StyleGuide;
